6567503bf910bc5a0c14656d3a4d1d8252ec9061
BarbaraBontempo <barbarabontempo94@gmail.com>
2021-02-17 11:23:49 -0500

    solution to capitalize

--- exercises/capitalize/index.js
+++ exercises/capitalize/index.js
@@ -7,6 +7,32 @@
 //   capitalize('a lazy fox') --> 'A Lazy Fox'
 //   capitalize('look, it is working!') --> 'Look, It Is Working!'
 
-function capitalize(str) {}
+function capitalize(str) {
+  //split input string by spaces to get an array
+  let strArr = str.split(" ");
+  //create an empty array for our results
+  let result = [];
+
+  //iterate through the strArr and for each word in the array
+  for (let word of strArr) {
+    //uppercase the first letter, join first letter with rest of the string, and push into results
+    result.push(word[0].toUpperCase() + word.slice(1));
+  }
+  //return results, but we want a string so don't forget to join(" ")
+  return result.join(" ");
+}
+
+// function capitalize(str) {
+//   //create result which is the first character of the input string capitalized
+//   let result = str[0].toUpperCase();
+//   for (let i = 1; i < str.length; i++) {
+//     if (str[i - 1] === " ") {
+//       result += str[i].toUpperCase();
+//     } else {
+//       result += str[i];
+//     }
+//   }
+//   return result;
+// }
 
 module.exports = capitalize;
