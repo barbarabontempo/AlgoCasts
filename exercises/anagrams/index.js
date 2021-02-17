38180b570c5accce34ac2bc69359a82c3a9ecb12
BarbaraBontempo <barbarabontempo94@gmail.com>
2021-02-17 11:00:03 -0500

    anagrams solution

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,6 +8,36 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+//SOLUTION 1:
+// function anagrams(stringA, stringB) {
+//   if (stringA === stringB) return true;
+//   let cleanStrA = stringCounter(stringA);
+//   let cleanStrB = stringCounter(stringB);
+
+//   if (Object.keys(cleanStrA).length !== Object.keys(cleanStrB).length)
+//     return false;
+
+//   for (let key in cleanStrA) {
+//     if (cleanStrA[key] !== cleanStrB[key]) return false;
+//   }
+//   return true;
+// }
+// //helper function to make our frequency counter
+// function stringCounter(str) {
+//   let obj = {};
+//   for (let char of str.replace(/[^\w]/g, "").toLowerCase()) {
+//     obj[char] = obj[char] + 1 || 1;
+//   }
+//   return obj;
+// }
+
+//SOLUTION 2:
+function anagrams(stringA, stringB) {
+  return stringCleaner(stringA) === stringCleaner(stringB);
+}
+//helper function to make our frequency counter
+function stringCleaner(str) {
+  return str.replace(/[^\w]/g, "").toLowerCase().split("").sort().join("");
+}
 
 module.exports = anagrams;
--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,37 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+// function chunk(array, size) {
+//   //create an empty array
+//   let results = [];
+
+//   //iterate through given array
+
+//   for (let i = 0; i < array.length; i++) {
+//     //for each element retrieve the last element in results
+//     let last = results[results.length - 1];
+//     //if last does not exist or if last.length === size,
+//     if (!last || last.length === size) {
+//       //push a new chunk into results with the current element
+//       console.log("check", array[i]);
+//       results.push([array[i]]); //push as an element inside an array
+//     } else {
+//       //else add the current element into last chunk
+//       last.push(array[i]);
+//     }
+//   }
+//   return results;
+// }
+
+function chunk(array, size) {
+  let results = [];
+
+  //iterate entire array
+  for (let i = 0; i < array.length; ) {
+    results.push(array.slice(i, size + i));
+    i = i + size;
+  }
+  return results;
+}
 
 module.exports = chunk;
